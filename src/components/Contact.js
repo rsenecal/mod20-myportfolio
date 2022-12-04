8795d9926efab5271d3c4cf8caf88cf20dfd3b25
rsenecal <roodysenecal@gmail.com>
2022-12-04 18:29:49 -0500

    Fixing title of app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     return(
     <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">If you're in search for a great developer with years of experience managing a remote team. </p>
         <form action="#" className="space-y-8">
             <div>
