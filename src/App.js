ae9bda86acd6e5ab986eb483bfca095a1eee7615
rsenecal <roodysenecal@gmail.com>
2022-12-07 18:39:39 -0500

    Getting HashRouter working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from 'react';
+
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+// import Main from './components/Main';
 
 // import './App.css';
 
 function App() {
   return (
-  <>
 
-    <main className="bg-gray-100 px-10 md:px-20 lg:px:40">
+  <main className="bg-gray-100 px-10 md:px-20 lg:px:40">
     <section className="min-h-screen">
       <Navbar />
+      {/* <Main /> */}
       <About />
       <Skills />
       <Projects />
       <Contact />
+
     </section>
   </main>
-  </>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,34 @@
 
 import { AiOutlineUser } from "react-icons/ai";
-// import { HashLink } from "react-router-hash-link";
-// import { BrowserRouter } from "react-router-dom";
-
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
 
     return(
-   
-    <>
-            <section>
-                <nav className='py-10 mb-12 flex justify-between'>
-                
-                    <ul className='flex items-center gap-10 px-10'>
-                 
-                        <li className='text-sm text-orange-600'> skillss</li>
-                        <li className='text-sm text-orange-600'> projects</li>
-                        <li className='text-sm text-orange-600'> Contact</li>
-                        <li>
-                            <AiOutlineUser className="cursor-pointer text-2xl"/>
-                           
-                        </li>
-                        {/* <li>
-                            <a className="bg-gradient-to-r  from-yellow-300 to-red-300 text-white px-4 py-2 rounded-md ml-8" href="#home">
-                                Portfolio
-                                </a>
-                        </li> */}
-
-                       
-                    </ul>
-                </nav>
-            </section>
+  
+    <section>
+        <nav className='py-10 mb-12 flex justify-between'>
+        
+            <ul className='flex items-center gap-10 px-10'>
     
-    </>
+                <li className='text-sm text-orange-600'><Link to='/skills'> Skills</Link></li>
+                <li className='text-sm text-orange-600'><Link to='/projects'> Projects</Link></li>
+                <li className='text-sm text-orange-600'><Link to='/contact'> Contact</Link></li>
+                <li>
+                    <AiOutlineUser className="cursor-pointer text-2xl"/>
+                    
+                </li>
+                {/* <li>
+                    <a className="bg-gradient-to-r  from-yellow-300 to-red-300 text-white px-4 py-2 rounded-md ml-8" href="#home">
+                        Portfolio
+                        </a>
+                </li> */}
+
+                
+            </ul>
+ 
+        </nav>
+
+    </section>
+    
     )
 }
